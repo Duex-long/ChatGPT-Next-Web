e73e8f4257ec203b302db936437180e42f54e49f
qinglong.shang.onsite <qinglong.shang.onsite@garena.com>
2024-04-15 17:24:46 +0800

    feat: 新增标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          乐子GPT
+          乐子狗屁通
         </div>
         <div className={styles["sidebar-sub-title"]}>《狠狠的让他上班。》</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
